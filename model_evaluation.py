582d331a5e87b80d15b721d1798f0bed23ae8bc4
PAPAGEORGIOU <giorgos.papageorgiou@cri-group.eu>
2024-01-07 18:17:17 +0200

    Add Mean Square Error and absolute error

--- model_evaluation.py
+++ model_evaluation.py
@@ -7,5 +7,5 @@ def evaluate_models(models, X, Y, dataset_type):
         accuracy = metrics.accuracy_score(Y, Y_pred)
         print("Accuracy score of {} on {} dataset: {:.2f}%".format(model.__class__.__name__, dataset_type, accuracy * 100))
         print("Mean Absolute Error: {}", metrics.mean_absolute_error(Y, Y_pred))
-        print("Mean Absolute Error: {}", metrics.mean_squared_error(Y, Y_pred))
+        print("Mean Squared Error: {}", metrics.mean_squared_error(Y, Y_pred))
 
